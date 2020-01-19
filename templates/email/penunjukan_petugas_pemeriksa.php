4a82ff307d5ac13f9f98dec2f404f708c0656cc1
Antonius Yonathan <atomicbomber.email@gmail.com>
2020-01-19 17:36:58 +0700

    Updates.

--- templates/email/penunjukan_petugas_pemeriksa.php
+++ templates/email/penunjukan_petugas_pemeriksa.php
@@ -1,6 +1,7 @@
 <p>
     <?= $nama_petugas ?>, Anda telah ditunjuk untuk melakukan pemeriksaan sampel Ikan Pari/Hiu Milik: <?= $nama_pemohon ?>. </p>
-
+    <br>
+    Tanggal <?= $tanggal_pemeriksaan ?? '' ?>
 <p>
     Untuk Pengisian Hasil Pemeriksaan Anda dapat mengunjungi <a href='<?= $target_url ?>'>  <?= $target_url ?> </a> : <br>
     username : <?= $username ?>  <br>
